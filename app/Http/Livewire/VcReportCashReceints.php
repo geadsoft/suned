cd70eb6115e41875b76d94ed3ab12d439f5b43f4
CGalarza <christianwgalarza@gmail.com>
2023-03-06 21:01:35 -0500

    filtrar nombre recibo caja

--- app/Http/Livewire/VcReportCashReceints.php
+++ app/Http/Livewire/VcReportCashReceints.php
@@ -73,7 +73,9 @@ class VcReportCashReceints extends Component
         ->join("tm_matriculas","tm_matriculas.id","=","tr_deudas_cabs.matricula_id")
         ->join("tm_personas","tm_personas.id","=","tm_matriculas.estudiante_id")
         ->join("tm_cursos","tm_cursos.id","=","tm_matriculas.curso_id")
-        ->join("tm_servicios","tm_servicios.id","=","tm_cursos.servicio_id")   
+        ->join("tm_servicios","tm_servicios.id","=","tm_cursos.servicio_id")
+        ->where('tr_deudas_dets.tipo','<>','DES')
+        ->where('tr_cobros_cabs.tipo','=','CP')   
         ->when($this->filters['srv_nombre'],function($query){
             return $query->where('tm_personas.nombres','like','%'.$this->filters['srv_nombre'].'%')
                         ->orWhere('tm_personas.apellidos','like','%'.$this->filters['srv_nombre'].'%');
@@ -87,8 +89,6 @@ class VcReportCashReceints extends Component
         ->when($this->filters['srv_fecha'],function($query){
             return $query->where('tr_cobros_cabs.fecha',"{$this->filters['srv_fecha']}");
         }) 
-        ->where('tr_deudas_dets.tipo','<>','DES')
-        ->where('tr_cobros_cabs.tipo','=','CP')
         ->select('tr_cobros_cabs.documento', 'tm_personas.nombres', 'tm_personas.apellidos', 'tm_servicios.descripcion', 'tm_cursos.paralelo', 'detalle', 'tipopago', 'saldo','credito', 'descuento', 'tr_deudas_dets.valor as pago',  'tr_cobros_cabs.usuario', 'tr_cobros_dets.referencia', 'entidad_id')
         ->orderBy('tr_cobros_cabs.fecha')
         ->paginate(15);
