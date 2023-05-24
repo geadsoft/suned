e611ec32dae86300265aaebb5a1c1c6849b09c01
ontime <christianwgalarza@gmail.com>
2023-05-23 23:10:43 -0500

    ficha estudiante

--- app/Http/Livewire/VcPersons.php
+++ app/Http/Livewire/VcPersons.php
@@ -141,7 +141,7 @@ class VcPersons extends Component
         ->join("tm_generalidades as g","g.id","=","m.modalidad_id")
         ->join("tm_generalidades as g2","g2.id","=","tm_personas.nacionalidad_id")
         ->when($this->filters['srv_nombre'],function($query){
-            return $query->whereRaw("concat(p.apellidos,' ',p.nombres) LIKE '%".$this->filters['srv_nombre']."%'");
+            return $query->whereRaw("concat(tm_personas.apellidos,' ',tm_personas.nombres) LIKE '%".$this->filters['srv_nombre']."%'");
         })
         ->when($this->filters['srv_periodo'],function($query){
             return $query->where('m.periodo_id',"{$this->filters['srv_periodo']}");
