97407e09de70e8ae5a3688863322c7a82a319456
Christian Galarza <christianwgalarza@gmail.com>
2023-11-27 10:03:29 -0500

    solicitudes

--- app/Http/Livewire/VcSolicitudes.php
+++ app/Http/Livewire/VcSolicitudes.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire;
 
 use App\Models\TmReportes;
+use App\Models\TmSedes;
 
 use Livewire\Component;
 use PDF;
@@ -10,10 +11,131 @@ use PDF;
 class VcSolicitudes extends Component
 {
     public $fecha,$documento,$nombres,$nui;
+    public $solicitud=[];
+
+    public function mount(){
+        $this->loadData();
+    }
 
     public function render()
     {
         return view('livewire.vc-solicitudes');
+    }
+
+    public function loadData(){
+        $objData['categoria'] = 'C';
+        $objData['subcategoria'] = 'Matricula';
+        $objData['periodo'] = '';
+        $objData['curso'] = '';
+        $objData['tiempo'] = '2 dias laborales';
+        array_push($this->solicitud, $objData);
+        $objData['categoria'] = 'C';
+        $objData['subcategoria'] = 'Conducta';
+        $objData['periodo'] = '';
+        $objData['curso'] = '';
+        $tobjData['tiempo'] = '2 dias laborales';
+        array_push($this->solicitud, $objData);
+        $objData['categoria'] = 'C';
+        $objData['subcategoria'] = 'Asistencia';
+        $objData['periodo'] = '';
+        $objData['curso'] = '';
+        $objData['tiempo'] = '2 dias laborales';
+        array_push($this->solicitud, $objData);
+        $objData['categoria'] = 'C';
+        $objData['subcategoria'] = 'Aprovechamiento';
+        $objData['periodo'] = '';
+        $objData['curso'] = '';
+        $objData['tiempo'] = '2 dias laborales';
+        array_push($this->solicitud, $objData);
+        $objData['categoria'] = 'C';
+        $objData['subcategoria'] = 'Libreta Escolar';
+        $objData['periodo'] = '';
+        $objData['curso'] = '';
+        $objData['tiempo'] = '2 dias laborales';
+        array_push($this->solicitud, $objData);
+        $objData['categoria'] = 'C';
+        $objData['subcategoria'] = 'Promoción';
+        $tobjData['periodo'] = '';
+        $objData['curso'] = '';
+        $objData['tiempo'] = '8 dias laborales';
+        array_push($this->solicitud, $objData);
+        $objData['categoria'] = 'C';
+        $objData['subcategoria'] = 'Pase Reglamentario';
+        $objData['periodo'] = '';
+        $objData['curso'] = '';
+        $objData['tiempo'] = '2 dias laborales';
+        array_push($this->solicitud, $objData);
+        $objData['categoria'] = 'C';
+        $objData['subcategoria'] = 'Retiro de Expediente';
+        $objData['periodo'] = '';
+        $objData['curso'] = '';
+        $objData['tiempo'] = '15 dias laborales';
+        array_push($this->solicitud, $objData);
+
+        $objData['categoria'] = 'E';
+        $objData['subcategoria'] = 'Participacion Estudiantil'."\n".'120 Horas';
+        $objData['periodo'] = '';
+        $objData['curso'] = '';
+        $objData['tiempo'] = '8 dias laborales';
+        array_push($this->solicitud, $objData);
+        $objData['categoria'] = 'E';
+        $objData['subcategoria'] = 'Participacion Estudiantil'."\n".'100 Horas';
+        $objData['periodo'] = '';
+        $objData['curso'] = '';
+        $objData['tiempo'] = '8 dias laborales';
+        array_push($this->solicitud, $objData);
+        $objData['categoria'] = 'E';
+        $objData['subcategoria'] = 'Participacion Estudiantil'."\n".'80 Horas';
+        $objData['periodo'] = '';
+        $objData['curso'] = '';
+        $objData['tiempo'] = '8 dias laborales';
+        array_push($this->solicitud, $objData);
+        $objData['categoria'] = 'E';
+        $objData['subcategoria'] = 'Pasantias';
+        $objData['periodo'] = '';
+        $objData['curso'] = '';
+        $objData['tiempo'] = '2 dias laborales';
+        array_push($this->solicitud, $objData);
+        
+        $objData['categoria'] = 'G';
+        $objData['subcategoria'] = 'Acta Original'."\n".'2000-2021';
+        $objData['periodo'] = '';
+        $objData['curso'] = '';
+        $objData['tiempo'] = '8 dias laborales';
+        array_push($this->solicitud, $objData);
+        $objData['categoria'] = 'G';
+        $objData['subcategoria'] = 'Duplicado Acta'."\n".'2000-2021';
+        $objData['periodo'] = '';
+        $objData['curso'] = '';
+        $objData['tiempo'] = '8 dias laborales';
+        array_push($this->solicitud, $objData);
+        $objData['categoria'] = 'G';
+        $objData['subcategoria'] = 'Acta Original'."\n".'1984-1999';
+        $objData['periodo'] = '';
+        $objData['curso'] = '';
+        $objData['tiempo'] = '8 dias laborales';
+        array_push($this->solicitud, $objData);
+        $objData['categoria'] = 'G';
+        $objData['subcategoria'] = 'Duplicado Acta'."\n".'1984-1999';
+        $objData['periodo'] = '';
+        $objData['curso'] = '';
+        $objData['tiempo'] = '8 dias laborales';
+        array_push($this->solicitud, $objData);
+        $objData['categoria'] = 'G';
+        $objData['subcategoria'] = 'Título de Bachiller'."\n".'2000-2021';
+        $objData['periodo'] = '';
+        $objData['curso'] = '';
+        $objData['tiempo'] = '8 dias laborales';
+        array_push($this->solicitud, $objData);
+        $objData['categoria'] = 'G';
+        $objData['subcategoria'] = 'Título de Bachiller'."\n".'1984-1999';;
+        $objData['periodo'] = '';
+        $objData['curso'] = '';
+        $objData['tiempo'] = '15 dias laborales';
+        array_push($this->solicitud, $objData);
+
+        return $this->solicitud;
+
     }
 
     public function print(){
@@ -24,10 +146,14 @@ class VcSolicitudes extends Component
 
     public function printPDF()
     {   
-        $data  = TmReportes::find(8);
-        
+        $data    = TmReportes::find(7);
+        $formato = $this->loadData(); 
+        $sede = TmSedes::orderBy('id','desc')->first();
+
         $pdf = PDF::loadView('reports/solicitudes',[
+            'sede'  => $sede,
             'data'  => $data,
+            'solicitud' => $formato,
         ]);
 
         return $pdf->setPaper('a4')->stream('Solicitud.pdf');
--- resources/views/reports/solicitudes.blade.php
+++ resources/views/reports/solicitudes.blade.php
@@ -20,18 +20,20 @@
     <section style ="margin-top: -110px;">
             <table cellpadding="0" cellspancing="0" width="100%" style="font-size:13px">
                 <tr>
-                    <td width="100%" style="vertical-align: top; padding-top: 8px">
-                        <img src="../public/assets/images/AmericanSchooll.jpg" height="80px">
-                    </td>        
+                    <td width="70%" style="vertical-align: top; padding-top: 8px">
+                        <img src="../public/assets/images/AmericanSchooll.jpg" height="60px">
+                    </td>
+                    <td width="30%" class="text-center">
+                        <p style="line-height: 200%" class="text-justify" style="font-size:8px; margin-top:0; margin-bottom:0;">
+                            {{$sede['direccion']}}<br>
+                            Telf.:{{$sede['telefono']}}<br>
+                            {{$sede['email']}}
+                            {{$sede['website']}}
+                        </p>
+                    </td>
                 </tr>
-                <div class="mb-3">
-                </div>
-                <td width="70%" class="text-center" style="vertical-align: top; padding-top: 10px">
-                    <span style="font-size: 12px" class="text-primary"><strong>Cdla Vernza</strong></span>
-                    <div class="mb-3">
-                    </div>
-                </td>
             </table>
+            <br>
             <table cellpadding="0" cellspacing="0" class="" width="100%">
                 <tr style="font-size:12px">
                     <td width="5%">
@@ -54,7 +56,7 @@
                                 <td>De mi consideración:</td>
                             </tr>
                             <tr>
-                                <td><p style="line-height: 200%" class="text-justify"><br><br>
+                                <td><p style="line-height: 200%" class="text-justify">
                                         Yo, {{$data['nombres']}} C.I. {{$data['identificacion']}}. Me dirijo a ustedes señores del Consejo Ejecutivo con la
                                         finalidad de solicitar:
                                     </p>
@@ -62,21 +64,20 @@
                             </tr>
                         </table>
                     </td>
-                    <td width="5%">
-                    </td> 
+                    <td width="5%"></td> 
                 </tr>               
             </table>
     </section>
     <section>
         <table cellpadding="0" cellspancing="0" width="100%">
         <tbody>                
-            <tr style="font-size:13px">  
+            <tr style="font-size:11px">  
                 <td width="5%">
                 </td>
                 <td width="90%">               
                     <table width="100%" style="font-size:10px;" border="1">
                         <thead>
-                            <tr style="font-size:13px">
+                            <tr style="font-size:11px">
                                 <th width="5%" class="text-center">Categoria</th>
                                 <th width="20%" class="text-center">Subcategoria</th>
                                 <th width="20%" class="text-center">Periodo Lectivo</th>
@@ -85,69 +86,108 @@
                             </tr>
                         </thead>
                         <tbody>
-                            <tr style="font-size:13px">
-                                <td rowspan=8>
-                                    <p  style="transform: rotate(270deg);">CERTIFICADOS
-                                    </p>
-                                </td>
-                                <td>Matricula</td>
-                                <td></td>
-                                <td></td>
-                                <td>2 dias laborales</td>
-                            </tr>
-                            <tr style="font-size:13px">
-                                <td>Conducta</td>
-                                <td></td>
-                                <td></td>
-                                <td>2 dias laborales</td>
-                            </tr>
-                            <tr style="font-size:13px">
-                                <td>Asistencia</td>
-                                <td></td>
-                                <td></td>
-                                <td>2 dias laborales</td>
-                            </tr>
-                            <tr style="font-size:13px">
-                                <td>Aprovechamiento</td>
-                                <td></td>
-                                <td></td>
-                                <td>2 dias laborales</td>
-                            </tr>
-                            <tr style="font-size:13px">
-                                <td>Libreta Escolar</td>
-                                <td></td>
-                                <td></td>
-                                <td>2 dias laborales</td>
-                            </tr>
-                            <tr style="font-size:13px">
-                                <td>Promoción</td>
-                                <td></td>
-                                <td></td>
-                                <td>2 dias laborales</td>
-                            </tr>
-                            <tr style="font-size:13px">
-                                <td>Pase Reglamentario</td>
-                                <td></td>
-                                <td></td>
-                                <td>2 dias laborales</td>
-                            </tr>
-                            <tr style="font-size:13px">
-                                <td>Retiro de Expediente</td>
-                                <td></td>
-                                <td></td>
-                                <td>2 dias laborales</td>
-                            </tr>
+                            @foreach ($solicitud as $key => $recno)
+                                @if ($recno['categoria']=='C')
+                                <tr style="font-size:11px">
+                                    @if ($key==0)
+                                    <td rowspan=8>
+                                        <p  style="transform: rotate(270deg);">CERTIFICADOS
+                                        </p>
+                                    </td>
+                                    @endif
+                                    <td>{{$recno['subcategoria']}}</td>
+                                    <td>{{$recno['periodo']}}</td>
+                                    <td>{{$recno['curso']}}</td>
+                                    <td>{{$recno['tiempo']}}</td>
+                                </tr>
+                                @endif
+                                @if ($recno['categoria']=='E')
+                                @if ($key==8)
+                                <tr style="font-size:11px">
+                                    <td class="text-center"><strong>Categoría</strong></td>
+                                    <td class="text-center"><strong>Subcategoría</strong></td>
+                                    <td class="text-center"><strong>Periodo Lectivo</strong></td>
+                                    <td class="text-center"><strong>Especialidad</strong></td>
+                                    <td class="text-center"><strong>Tiempo de Entrega</strong></td>
+                                </tr>
+                                @endif
+                                <tr style="font-size:11px">
+                                    @if ($key==8)
+                                    <td rowspan=4>
+                                        <p style="transform: rotate(270deg);">EXTRAS
+                                        </p>
+                                    </td>
+                                    @endif
+                                    <td>{{$recno['subcategoria']}}</td>
+                                    <td>{{$recno['periodo']}}</td>
+                                    <td>{{$recno['curso']}}</td>
+                                    <td>{{$recno['tiempo']}}</td>
+                                </tr>
+                                @endif
+                                @if ($recno['categoria']=='G')
+                                @if ($key==12)
+                                <tr style="font-size:11px">
+                                    <td class="text-center"><strong>Categoría</strong></td>
+                                    <td class="text-center"><strong>Subcategoría</strong></td>
+                                    <td class="text-center"><strong>Periodo Lectivo</strong></td>
+                                    <td class="text-center"><strong>Especialidad</strong></td>
+                                    <td class="text-center"><strong>Tiempo de Entrega</strong></td>
+                                </tr>
+                                @endif
+                                <tr style="font-size:11px">
+                                    @if ($key==12)
+                                    <td rowspan=6>
+                                        <p style="transform: rotate(270deg);">GRADUADOS
+                                        </p>
+                                    </td>
+                                    @endif
+                                    <td>{{$recno['subcategoria']}}</td>
+                                    <td>{{$recno['periodo']}}</td>
+                                    <td>{{$recno['curso']}}</td>
+                                    <td>{{$recno['tiempo']}}</td>
+                                </tr>
+                                @endif
+                            @endforeach
                         </tbody>
                     </table>
                 </td>
                 <td width="5%">
                 </td>
             </tr>
-                            </tbody>
+        </tbody>
         </table>
     </section>
-
-
+    <section>
+        <table cellpadding="0" cellspacing="0" class="" width="100%">
+            <tr style="font-size:12px">
+                <td width="5%">
+                </td>
+                <td width="90%">
+                    <table width="100%">
+                        <tr>
+                            <td><p style="line-height: 200%" class="text-justify">
+                                    <strong>Observaciones:</strong>
+                                </p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td width="40%"><p style="line-height: 200%" class="text-justify">
+                                    <strong>Atentamente,</strong>
+                            </td>
+                            <td width="60%" style="border: 1px solid;">
+                                <p style="line-height: 200%" class="text-justify">
+                                    Fecha de Entrega: <br>
+                                    Nombre del Servidor: 
+                                </p>
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+                <td width="5%">
+                </td> 
+            </tr>          
+        </table>
+    </section>
 
     <div style="position: absolute;
       display: inline-block;
@@ -155,35 +195,82 @@
       width: 100%;
       height: 150px;">
         <section>
-
-            <table cellpadding="0" cellspacing="0" class="" width="100%">
+        <table cellpadding="0" cellspacing="0" class="" width="100%">
+            <tr>
+            <td width="5%">
+            </td>
+            <td width="90%">
+            
+            <table>
                 <tr style="font-size:12px">
                     <td width="50%" class="text-center">
                         <span><strong>________________________________</strong></span>
                     </td>
                     <td width="50%" class="text-center">
-                        <span><strong>________________________________</strong></span>
+                       
                     </td>
                 </tr>
-                <tr style="font-size:13px">
-                    <td width="50%" class="text-center">
-                        <span>{{$data['rector']}}</span>
+                <tr style="font-size:12px">
+                    <td width="50%">
+                        <span><strong>Firma</strong></span>
                     </td>
                     <td width="50%" class="text-center">
-                        <span>{{$data['secretaria']}}</span>
-                    </td>
-                </tr>
-                <tr style="font-size:13px">
-                    <td width="50%" class="text-center">
-                        <span><strong>RECTOR</strong></span>
-                    </td>
-                    <td width="50%" class="text-center">
-                        <span><strong>SECRETARIA</strong></span>
+                        
                     </td>
                 </tr>
             </table>
+            </td>
+            <td width="5%">
+            </td>
+            </tr>
+        </table>
         </section>
     </div>
+    <section>
+        <br>
+        <table cellpadding="0" cellspacing="0" class="" width="100%">
+            <tr style="font-size:12px">
+                <td width="5%">
+                </td>
+                <td width="90%">
+                    <table width="100%">
+                        <tr>
+                            <td colspan=2>
+                            </td>
+                            <td width="20%">Solicitado de Forma: 
+                            </td>
+                        </tr>
+                        <tr>
+                            <td width="10%" style="border: 1px solid;">Celular:
+                            </td>
+                            <td width="70%" style="border: 1px solid;">
+                            </td>
+                            <td width="20%" style="border: 1px solid;">Presencial 
+                            </td>
+                        </tr>
+                        <tr>
+                            <td width="10%" style="border: 1px solid;">Teléfono:
+                            </td>
+                            <td width="70%" style="border: 1px solid;">
+                            </td>
+                            <td width="20%" style="border: 1px solid;">Correo 
+                            </td>
+                        </tr>
+                        <tr>
+                            <td width="10%" style="border: 1px solid;">E-mail:
+                            </td>
+                            <td width="70%" style="border: 1px solid;">
+                            </td>
+                            <td width="20%" style="border: 1px solid;">Telefononicamente 
+                            </td>
+                        </tr>
+                    </table>
+                </td>
+                <td width="5%">
+                </td>
+            </tr>              
+        </table>
+    </section>
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
