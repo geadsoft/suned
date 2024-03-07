25b16396121adcf3422679ba2f88e3abaf7a6355
Christian Galarza <christianwgalarza@gmail.com>
2024-03-07 15:44:23 -0500

    grafico ingresos mes

--- app/Http/Livewire/VcStatisticalGraphs.php
+++ app/Http/Livewire/VcStatisticalGraphs.php
@@ -269,10 +269,10 @@ class VcStatisticalGraphs extends Component
         ->where('tr_cobros_cabs.tipo','CP')
         ->where('d.tipo','PAG')
         ->where('d.estado','P')
-        ->whereRaw('year(tr_cobros_cabs.fecha) = '.$this->filters['anioingreso'])
-        ->whereRaw('month(tr_cobros_cabs.fecha) <= '.$this->filters['mesingreso'])
-        ->selectRaw('month(tr_cobros_cabs.fecha) as mes,  sum(d.valor) AS monto')
-        ->groupbyRaw('month(tr_cobros_cabs.fecha)')
+        ->whereRaw('year(tr_cobros_cabs.fechapago) = '.$this->filters['anioingreso'])
+        ->whereRaw('month(tr_cobros_cabs.fechapago) <= '.$this->filters['mesingreso'])
+        ->selectRaw('month(tr_cobros_cabs.fechapago) as mes,  sum(d.valor) AS monto')
+        ->groupbyRaw('month(tr_cobros_cabs.fechapago)')
         ->orderby('mes','desc')
         ->get()->toArray();
 
