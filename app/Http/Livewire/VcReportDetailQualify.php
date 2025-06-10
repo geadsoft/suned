11bbca44a66b0be4e687711a4995a80c8118673f
suned <christianwgalarza@gmail.com>
2025-06-09 23:34:26 -0500

    reporte detallado

--- app/Http/Livewire/VcReportDetailQualify.php
+++ app/Http/Livewire/VcReportDetailQualify.php
@@ -246,7 +246,11 @@ class VcReportDetailQualify extends Component
                     ->select("n.*")
                     ->first();
                                         
-                    $nota =  $notas['nota'];
+                    $nota =  0;
+                    if(is_null($nota)){
+                        $nota = $notas['nota'];
+                    }
+
                     $col = $key2.$key3;
                     $this->tblrecords[$key][$col] = floatval($nota);
                     $suma = $suma + floatval($nota);
