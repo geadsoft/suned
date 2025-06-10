4ca19091e4e4b87fe91c7310e351c9a21fff5053
suned <christianwgalarza@gmail.com>
2025-06-10 01:53:12 -0500

    actividades

--- resources/views/livewire/vc-student-activities.blade.php
+++ resources/views/livewire/vc-student-activities.blade.php
@@ -102,7 +102,7 @@
                                                                 , <small class="text-muted"> {{date('H:i',strtotime($record->fecha))}}</small></td>
                                                         <td class="amount">{{$record->nota}}</td>
                                                         <td class="status">
-                                                            @if($record->estado=='A')
+                                                            @if($record->estado=='A' && is_null($record->fechaentrega))
                                                             <span class="badge badge-soft-success text-uppercase">@lang('status.'.($record->estado))</span>
                                                             @else
                                                                 @if (is_null($record->nota) && is_null($record->fechaentrega))
