56e36fc80c54149021a031c9ea4c2fcf07a6b107
suned <christianwgalarza@gmail.com>
2025-06-24 21:17:49 -0500

    calificar actividad

--- app/Http/Livewire/VcQualifyActivity.php
+++ app/Http/Livewire/VcQualifyActivity.php
@@ -186,6 +186,8 @@ class VcQualifyActivity extends Component
         }
         $this->tblrecords['ZZ']['promedio'] = 0.00;
 
+        dd($this->tblrecords);
+
     }
 
     public function asignarNotas(){
