5c18c17603c4c967d642fec7dc3ecac474e43d28
suned <christianwgalarza@gmail.com>
2025-08-05 21:00:12 -0500

    calificar actividad

--- app/Http/Livewire/VcQualifyActivity.php
+++ app/Http/Livewire/VcQualifyActivity.php
@@ -175,8 +175,9 @@ class VcQualifyActivity extends Component
 
         $this->personas = TmHorariosDocentes::query()
         ->join("tm_horarios as h","h.id","=","tm_horarios_docentes.horario_id")
-        ->join(DB::raw("(select estudiante_id, modalidad_id, periodo_id, curso_id, estado 
+        ->join(DB::raw("(select m.estudiante_id, m.modalidad_id, m.periodo_id, m.curso_id, m.estado 
         from tm_matriculas m 
+        left join tm_pase_cursos p on p.mtricula_id <> m.id
         where m.modalidad_id = ".$this->modalidadId."  and m.periodo_id = ".$this->periodoId."
         union all
         select m.estudiante_id, p.modalidad_id, m.periodo_id, p.curso_id, m.estado
