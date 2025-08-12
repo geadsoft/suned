cf628e25eefb4b653bea2eb5ec13538e190c7ac5
suned <christianwgalarza@gmail.com>
2025-08-12 12:57:07 -0500

    boletin calificacion

--- app/Http/Livewire/VcReportCard.php
+++ app/Http/Livewire/VcReportCard.php
@@ -435,12 +435,15 @@ class VcReportCard extends Component
                     $this->tblrecords[$key1][$key2]['promedio'] = round($promedio/($countprm), 2);  
                 }
 
-                $nota70 = round($this->tblrecords[$key1][$key2]['promedio']*0.70,2);
-                $nota30 = round($this->tblrecords[$key1][$key2]['examen']*0.30,2);
-                
-                $this->tblrecords[$key1][$key2]['nota70'] = round($nota70, 2);
-                $this->tblrecords[$key1][$key2]['nota30'] = round($nota30, 2);
-                $this->tblrecords[$key1][$key2]['cuantitativo'] = $nota70+$nota30; 
+                if ($promedio>0){
+                    $nota70 = round($this->tblrecords[$key1][$key2]['promedio']*0.70,2);
+                    $nota30 = round($this->tblrecords[$key1][$key2]['examen']*0.30,2);
+                    
+                    $this->tblrecords[$key1][$key2]['nota70'] = round($nota70, 2);
+                    $this->tblrecords[$key1][$key2]['nota30'] = round($nota30, 2);
+                    $this->tblrecords[$key1][$key2]['cuantitativo'] = $nota70+$nota30; 
+                }
+
             }
         }
 
@@ -471,6 +474,7 @@ class VcReportCard extends Component
                 $nota30 += $recno['nota30'];
                 $promfinal += $recno['cuantitativo'];
             }
+            
             $this->tblrecords[$key]['ZZ']['AI-prom']  = round($aiprom/$count,2);
             $this->tblrecords[$key]['ZZ']['AG-prom']  = round($agprom/$count,2);
             $this->tblrecords[$key]['ZZ']['promedio'] = round($promedio/$count,2);
