38d816926d509b38addaa7aa9ed644413e3b47d4
ontime <christianwgalarza@gmail.com>
2026-02-19 00:10:15 -0500

    boletin final

--- app/Http/Livewire/VcFinalBulletin.php
+++ app/Http/Livewire/VcFinalBulletin.php
@@ -548,7 +548,7 @@ class VcFinalBulletin extends Component
                 $join->on('d.id', '=', 'tm_actividades.paralelo')
                     ->on('d.docente_id', '=', 'tm_actividades.docente_id');
             })
-            /*->join("tm_horarios as h","h.id","=","d.horario_id")
+            ->join("tm_horarios as h","h.id","=","d.horario_id")
             ->when(
                 $this->filters['paralelo']  && ($this->filters['paralelo_pase'] == 0),
                 function ($query) {
@@ -560,7 +560,7 @@ class VcFinalBulletin extends Component
                 function ($query) {
                     $query->where('h.curso_id', $this->filters['paralelo_pase']);
                 }
-            )*/
+            )
             ->when(!empty($this->filters['termino']), function($query) {
                 return $query->where('tm_actividades.termino', $this->filters['termino']);
             })
