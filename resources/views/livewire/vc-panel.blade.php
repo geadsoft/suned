03035f07b97c83e600cdae2b43a4622b37e3e694
ontime <christianwgalarza@gmail.com>
2026-04-01 21:48:56 -0500

    grafico

--- resources/views/livewire/vc-panel.blade.php
+++ resources/views/livewire/vc-panel.blade.php
@@ -13,7 +13,7 @@
                         <div class="mt-3 mt-lg-0">
                             <form action="javascript:void(0);">
                                 <div class="row g-3 mb-0 align-items-center">
-                                    <div class="col-sm-auto">
+                                    <!--<div class="col-sm-auto">
                                         <div class="input-group">
                                             <input type="text"
                                                 class="form-control border-0 dash-filter-picker shadow"
@@ -24,7 +24,7 @@
                                                 class="input-group-text bg-primary border-primary text-white">
                                                 <i class="ri-calendar-2-line"></i></div>
                                         </div>
-                                    </div>
+                                    </div>-->
                                     <!--end col-->
                                     <div class="col-auto">
                                         <button type="button" class="btn btn-soft-secondary"><i
