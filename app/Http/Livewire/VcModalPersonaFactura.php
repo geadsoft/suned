81ab0a255e2d91d2ec8e6a739104db75b06022ef
ontime <christianwgalarza@gmail.com>
2026-06-04 20:19:16 -0500

    buscar factura

--- app/Http/Livewire/VcModalPersonaFactura.php
+++ app/Http/Livewire/VcModalPersonaFactura.php
@@ -125,7 +125,7 @@ class VcModalPersonaFactura extends Component
         ->orderBy('m.modalidad_id')
         ->orderBy('pe.periodo','desc')
         ->orderBy('tm_personas.apellidos')
-        ->orderBy('tm_personas.nombres')
+        ->orderBy('tm_personas.nombre')
         ->get();
 
         return $tbldata;
