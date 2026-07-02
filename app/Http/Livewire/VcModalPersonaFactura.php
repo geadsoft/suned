a9b871f4ab9c1c28b39d7089176ee74075f101cc
ontime <christianwgalarza@gmail.com>
2026-07-01 21:52:03 -0500

    notas parcial

--- app/Http/Livewire/VcModalPersonaFactura.php
+++ app/Http/Livewire/VcModalPersonaFactura.php
@@ -55,7 +55,7 @@ class VcModalPersonaFactura extends Component
             $buscar = trim($this->filters['srv_nombre']);
 
             return $query->whereRaw(
-                "UPPER(CONCAT(tm_personas.apellidos, ' ', tm_personas.nombres)) LIKE ?",
+                "UPPER(CONCAT(trim(tm_personas.apellidos),' ',trim(tm_personas.nombres))) LIKE ?",
                 ['%' . strtoupper($buscar) . '%']
             );
         })
