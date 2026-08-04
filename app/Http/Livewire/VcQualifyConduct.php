09e3576643c89cbbb1e2cf765a819a79f6f24cb8
ontime <christianwgalarza@gmail.com>
2026-08-03 21:46:59 -0500

    registrar conducta

--- app/Http/Livewire/VcQualifyConduct.php
+++ app/Http/Livewire/VcQualifyConduct.php
@@ -71,6 +71,10 @@ class VcQualifyConduct extends Component
         // ========================
         $this->tbltermino = (clone $baseQuery)
             ->where('tipo', 'EA')
+            ->when($this->actividadId == 0, function ($query) {
+                $query->where('cerrar', 0);
+            })
+            ->orderBy('cerrar')
             ->orderBy('codigo')
             ->get();
 
